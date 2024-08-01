45c83cb2e19baa049a472e8d2f23c374b34e96c0
ManosGiannakakis <43854960+MGiannakakis@users.noreply.github.com>
2024-08-01 15:21:35 +0200

    Update Pair_Sym_Plastic_Network_Pulses_Sub_Norm.py

--- Networks/Pair_Sym_Plastic_Network_Pulses_Sub_Norm.py
+++ Networks/Pair_Sym_Plastic_Network_Pulses_Sub_Norm.py
@@ -163,37 +163,37 @@ def Network_get_Stats(W = 0.0,
     Noise_Synapses = Synapses(Noise, P, on_pre='g_ampa += w_input*gBarEx*nS')
     Noise_Synapses.connect(condition='i==j')
 
-  ########################
-  # Recurrent connections 
-  ########################
-  
-  # Inhibitory scaling
-  i_s = gBarEx/(gBarIn*(1-ExFrac))
-  
-  
-  if p > 0:
-      R_ee = Synapses(Pe, Pe, model='w: 1', on_pre='g_ampa += w*gBarEx*nS')
-      I, J, W = get_recurrent_connectivity(InputGroup[:NE], InputGroup[:NE],
-                                           p, p, w_ee_in, w_ee_out)
-      R_ee.connect(i=I, j=J)
-      R_ee.w = W
-  
-      R_ei = Synapses(Pe, Pi, model='w: 1', on_pre='g_ampa += w*gBarEx*nS')
-      I, J, W = get_recurrent_connectivity(InputGroup[:NE], InputGroup[NE:],
-                                           p, p, w_ei_in, w_ei_out)
-      R_ei.connect(i=I, j=J)
-      R_ei.w = W
-  
-      R_ie = Synapses(Pi, Pe, model='w: 1', on_pre='g_gaba += i_s*w*gBarIn*nS')
-      I, J, W = get_recurrent_connectivity(InputGroup[NE:], InputGroup[:NE],
-                                           p, p, w_ie_in, w_ie_out)
-      R_ie.connect(i=I, j=J)
-      R_ie.w = W
-  
-      R_ii = Synapses(Pi, Pi, model='w: 1', on_pre='g_gaba += i_s*w*gBarIn*nS')
-      I, J, W = get_recurrent_connectivity(InputGroup[NE:], InputGroup[NE:],
-                                           p, p, w_ii_in, w_ii_out)
-      R_ii.connect(i=I, j=J)
+    ########################
+    # Recurrent connections 
+    ########################
+    
+    # Inhibitory scaling
+    i_s = gBarEx/(gBarIn*(1-ExFrac))
+    
+    
+    if p > 0:
+        R_ee = Synapses(Pe, Pe, model='w: 1', on_pre='g_ampa += w*gBarEx*nS')
+        I, J, W = get_recurrent_connectivity(InputGroup[:NE], InputGroup[:NE],
+                                             p, p, w_ee_in, w_ee_out)
+        R_ee.connect(i=I, j=J)
+        R_ee.w = W
+    
+        R_ei = Synapses(Pe, Pi, model='w: 1', on_pre='g_ampa += w*gBarEx*nS')
+        I, J, W = get_recurrent_connectivity(InputGroup[:NE], InputGroup[NE:],
+                                             p, p, w_ei_in, w_ei_out)
+        R_ei.connect(i=I, j=J)
+        R_ei.w = W
+    
+        R_ie = Synapses(Pi, Pe, model='w: 1', on_pre='g_gaba += i_s*w*gBarIn*nS')
+        I, J, W = get_recurrent_connectivity(InputGroup[NE:], InputGroup[:NE],
+                                             p, p, w_ie_in, w_ie_out)
+        R_ie.connect(i=I, j=J)
+        R_ie.w = W
+    
+        R_ii = Synapses(Pi, Pi, model='w: 1', on_pre='g_gaba += i_s*w*gBarIn*nS')
+        I, J, W = get_recurrent_connectivity(InputGroup[NE:], InputGroup[NE:],
+                                             p, p, w_ii_in, w_ii_out)
+        R_ii.connect(i=I, j=J)
       R_ii.w = W
 
     #############################
